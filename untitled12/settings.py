95ca3621b5b10d4e82337202371f20ab678ec8ad
alikeb <50678498+alikeb@users.noreply.github.com>
2020-09-25 10:50:09 +0330

    bug fix

--- untitled12/settings.py
+++ untitled12/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddlewarepip install psycopg2-binary',
+    # 'django.contrib.sessions.middleware.SessionMiddlewarepip install psycopg2-binary',
+    "django.contrib.sessions.middleware.SessionMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
